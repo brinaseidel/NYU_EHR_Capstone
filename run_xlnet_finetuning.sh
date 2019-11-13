1111ede3f973f1601635f0874918c922d0db275e
Seidel <bs3743@bigpurple-ln2.cm.cluster>
2019-11-13 12:08:34 -0500

    Changed padding in pre-processing code and debugged evaluation saving code.

--- run_xlnet_finetuning.sh
+++ run_xlnet_finetuning.sh
@@ -3,10 +3,10 @@
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=2
 #SBATCH --mem-per-cpu=64G
-#SBATCH --time=9-5:00:00
+#SBATCH --time=9-12:00:00
 #SBATCH --partition=gpu8_long
 #SBATCH --gres=gpu:2
-#SBATCH --output=/gpfs/data/razavianlab/capstone19/logs/xlnet_finetune_full_256.log
+#SBATCH --output=/gpfs/data/razavianlab/capstone19/logs/xlnet_finetune_full_256_v2.log
 
 module load python/gpu/3.6.5
 module load gcc/4.9.3
@@ -17,7 +17,7 @@ echo -e "GPUS = $CUDA_VISIBLE_DEVICES\n"
 
 #python xlnet_finetuning.py --model_id xlnet_finetune --batch_size 32 --num_train_epochs 3 --seed 100 --fp16
 #python -u xlnet_finetuning.py --model_id xlnet_finetune_small256 --batch_size 32 --num_train_epochs 10 --seed 100 --fp16 --train_logging_step 1 --val_logging_step 10 --save_step 100000 --feature_save_dir small_256
-python xlnet_finetuning.py --model_id xlnet_finetune_full_256 --batch_size 32 --num_train_epochs 3 --seed 100 --fp16 --train_logging_step 1000 --val_logging_step 10000 --save_step 100000 --feature_save_dir full_train_256
+python xlnet_finetuning.py --model_id xlnet_finetune_full_256_v2 --batch_size 32 --num_train_epochs 3 --seed 100 --fp16 --train_logging_step 1000 --val_logging_step 20000 --save_step 100000 --feature_save_dir full_256
 
 
 
--- run_xlnet_preprocess.sh
+++ run_xlnet_preprocess.sh
@@ -3,10 +3,10 @@
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=2
 #SBATCH --mem-per-cpu=64G
-#SBATCH --time=1:00:00
+#SBATCH --time=5:00:00
 #SBATCH --partition=gpu4_short
 #SBATCH --gres=gpu:2
-#SBATCH --output=/gpfs/data/razavianlab/capstone19/logs/preprocess_small_256.log
+#SBATCH --output=/gpfs/data/razavianlab/capstone19/logs/preprocess_full_512_val_test.log
 
 module load python/gpu/3.6.5
 module load gcc/4.9.3
@@ -14,5 +14,6 @@ module load gcc/4.9.3
 pip install --user transformers
 
 echo -e "GPUS = $CUDA_VISIBLE_DEVICES\n"
-python xlnet_preprocess.py --filename "small.csv.gz" --set_type "train" --max_seq_length 256 --feature_save_dir "small_256"
-python xlnet_preprocess.py --filename "small.csv.gz" --set_type "val" --max_seq_length 256 --feature_save_dir "small_256"
+#python xlnet_preprocess.py --filename "train_cleaned.csv.gz" --set_type "train" --max_seq_length 512 --feature_save_dir "full_512"
+python xlnet_preprocess.py --filename "dev_cleaned.csv.gz" --set_type "val" --max_seq_length 512 --feature_save_dir "full_512"
+python xlnet_preprocess.py --filename "test_cleaned.csv.gz" --set_type "test" --max_seq_length 512 --feature_save_dir "full_512"
